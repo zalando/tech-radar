ce1ee00c63b32e95f4d0fdf1089101637c7e64a8
Gaetano Contaldi <gaetanocontaldi000@gmail.com>
2017-04-12 10:37:30 +0200

    use mtech spreadsheet

--- download.sh
+++ download.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+DAY=`date +%d`
 MONTH=`date +%m`
 YEAR=`date +%Y`
-FILE="data/${YEAR}_${MONTH}.tsv"
+FILE="data/${YEAR}_${MONTH}_${DAY}.tsv"
 
-curl "https://docs.google.com/spreadsheets/d/1HvsbibsBTBvZXDaJZWYyHwTxuA7a4eEbgcIKaPhuoxs/export?gid=0&format=tsv" -o "$FILE"
+curl "https://docs.google.com/spreadsheets/d/16gzAKq7rw49qJjLEp8dqbc_xP3ErAWTopXQH6ghtxxQ/export?gid=0&format=tsv" -o "$FILE"
 
 #remove first row
 tail -n +2 "$FILE" > "$FILE.tmp" && mv "$FILE.tmp" "$FILE"
