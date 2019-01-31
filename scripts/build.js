3d7746be9e8a7f8401ef2573874e4afb6c551013
Tim Doherty <tim.doherty@procore.com>
2019-01-30 16:53:30 -0800

    preserve license in minified lib; remove gh-pages index file

--- scripts/build.js
+++ scripts/build.js
@@ -1,7 +1,16 @@
 const Terser = require("terser");
 const fs = require("fs");
 
+// TODO set options for terser
+const options = {
+  output: {
+    comments: "some"
+  }
+};
 const version = process.env.npm_package_version;
-const result = Terser.minify(fs.readFileSync("./docs/radar.js", "utf8"));
+const result = Terser.minify(
+  fs.readFileSync("./docs/radar.js", "utf8"),
+  options
+);
 
 fs.writeFileSync(`./dist/radar-${version}.min.js`, result.code);
