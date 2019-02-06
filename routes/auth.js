bd3e0be12f16313b349e043c30fe9770bf4bbc96
Tim Doherty <tim.doherty@procore.com>
2019-02-06 13:35:16 -0800

    add state to auth router

--- routes/auth.js
+++ routes/auth.js
@@ -14,37 +14,40 @@ router.get(
 );
 
 // Perform the final stage of authentication and redirect to previously requested URL or '/user'
-// router.get(
-//   "/callback",
-//   passport.authenticate("auth0", { failureRedirect: "/login" }),
-//   function(req, res, next) {
-//     if (!req.user) {
-//       return res.redirect("/login");
-//     }
-//     const returnTo = req.session.returnTo;
-//     delete req.session.returnTo;
-//     res.redirect(returnTo || "/");
-//   }
-// );
-router.get("/callback", { state: "foobar" }, function(req, res, next) {
-  passport.authenticate("auth0", function(err, user, info) {
-    console.log({ err, user, info });
-    if (err) {
-      return next(err);
-    }
-    if (!user) {
+router.get(
+  "/callback",
+  passport.authenticate("auth0", {
+    state: "foobar",
+    failureRedirect: "/login"
+  }),
+  function(req, res, next) {
+    if (!req.user) {
       return res.redirect("/login");
     }
-    req.logIn(user, function(err) {
-      if (err) {
-        return next(err);
-      }
-      const returnTo = req.session.returnTo;
-      delete req.session.returnTo;
-      res.redirect(returnTo || "/");
-    });
-  })(req, res, next);
-});
+    const returnTo = req.session.returnTo;
+    delete req.session.returnTo;
+    res.redirect(returnTo || "/");
+  }
+);
+// router.get("/callback", { state: "foobar" }, function(req, res, next) {
+//   passport.authenticate("auth0", function(err, user, info) {
+//     console.log({ err, user, info });
+//     if (err) {
+//       return next(err);
+//     }
+//     if (!user) {
+//       return res.redirect("/login");
+//     }
+//     req.logIn(user, function(err) {
+//       if (err) {
+//         return next(err);
+//       }
+//       const returnTo = req.session.returnTo;
+//       delete req.session.returnTo;
+//       res.redirect(returnTo || "/");
+//     });
+//   })(req, res, next);
+// });
 
 // Perform session logout and redirect to homepage
 router.get("/logout", (req, res) => {
