020cb674bf486a1d189061f1574f497dc7725998
Tim Doherty <tim.doherty@procore.com>
2019-02-06 16:35:34 -0800

    distable oauth state

--- app.js
+++ app.js
@@ -23,8 +23,8 @@ const strategy = new Auth0Strategy(
     clientID: process.env.AUTH0_CLIENT_ID,
     clientSecret: process.env.AUTH0_CLIENT_SECRET,
     callbackURL:
-      process.env.AUTH0_CALLBACK_URL || "http://localhost:3000/callback",
-    state: true
+      process.env.AUTH0_CALLBACK_URL || "http://localhost:3000/callback"
+    // state: true
   },
   function(accessToken, refreshToken, extraParams, profile, done) {
     // accessToken is the token to call Auth0 API (not needed in the most cases)
