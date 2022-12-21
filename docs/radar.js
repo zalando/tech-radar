6699625e32218534bc5394fbb644cd2df37a4d31
Bartosz Ocytko <bocytko@gmail.com>
2022-12-21 15:35:20 +0100

    Adds more space in legend to avoid overlapping entries

--- docs/radar.js
+++ docs/radar.js
@@ -256,7 +256,7 @@ function radar_visualization(config) {
   }
 
   function legend_transform(quadrant, ring, index=null) {
-    var dx = ring < 2 ? 0 : 120;
+    var dx = ring < 2 ? 0 : 140;
     var dy = (index == null ? -16 : index * 12);
     if (ring % 2 === 1) {
       dy = dy + 36 + segmented[quadrant][ring-1].length * 12;
