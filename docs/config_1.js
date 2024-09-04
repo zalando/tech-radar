a531a02209bacd29d2fa7d42d326505095407323
timglabisch <glabisch@easybill.de>
2024-09-04 10:50:30 +0200

    revert usless changes

--- docs/config_1.js
+++ docs/config_1.js
@@ -2,9 +2,9 @@ window.config1 = {
     "date": "2024.06",
     "entries": [
     {
-        "quadrant": window.QUADRANT_TOP_RIGHT,
-        "ring": window.RING_TRIAL,
-        "label": "TEST! :)",
+        "quadrant": window.QUADRANT_BOTTOM_LEFT,
+        "ring": window.RING_ADOPT,
+        "label": "TEST!! :)",
         "active": true,
         "moved": 1
     },
