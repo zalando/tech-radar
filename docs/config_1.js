d95fee5054f523b64f85af5bc6101b393792d262
timglabisch <glabisch@easybill.de>
2024-09-04 11:19:20 +0200

    ...

--- docs/config_1.js
+++ docs/config_1.js
@@ -1,24 +1,47 @@
 window.config1 = {
     "date": "2024.06",
     "entries": [
-    {
-        "quadrant": window.QUADRANT_BOTTOM_RIGHT,
-        "ring": window.RING_ADOPT,
-        "label": "PHP",
-        "active": true,
-        "moved": 0
-    },{
-        "quadrant": window.QUADRANT_TOP_LEFT,
-        "ring": window.RING_HOLD,
-        "label": "Percona XtraDB Cluster",
-        "active": true,
-        "moved": 0
-    },{
-        "quadrant": window.QUADRANT_TOP_LEFT,
-        "ring": window.RING_TRIAL,
-        "label": "TiDB",
-        "active": true,
-        "moved": 0
-    },
-]
+        {
+            "quadrant": window.QUADRANT_BOTTOM_RIGHT,
+            "ring": window.RING_ADOPT,
+            "label": "PHP",
+            "active": true,
+            "moved": 0
+        },
+        {
+            "quadrant": window.QUADRANT_BOTTOM_LEFT,
+            "ring": window.RING_TRIAL,
+            "label": "Kubernetes (RKE2)",
+            "active": true,
+            "moved": 2
+        },
+        {
+            "quadrant": window.QUADRANT_TOP_LEFT,
+            "ring": window.RING_HOLD,
+            "label": "Percona XtraDB Cluster",
+            "active": true,
+            "moved": 0
+        },
+        {
+            "quadrant": window.QUADRANT_TOP_LEFT,
+            "ring": window.RING_TRIAL,
+            "label": "TiDB",
+            "active": true,
+            "moved": 0
+        },
+        {
+            "quadrant": window.QUADRANT_BOTTOM_LEFT,
+            "ring": window.RING_ADOPT,
+            "label": "NodeJS",
+            "active": true,
+            "moved": 0
+        },
+        {
+            "quadrant": window.QUADRANT_BOTTOM_LEFT,
+            "ring": window.RING_ASSES,
+            "label": "Deno",
+            "active": true,
+            "moved": 2,
+        },
+    ]
 }
