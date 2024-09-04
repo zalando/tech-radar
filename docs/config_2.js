f10552673d8cd5e49062f860a4364c4d3f0bf353
Christian Rodriguez Benthake <christian.rodriguez.benthake@easybill.de>
2024-09-04 11:49:12 +0200

    Team 2

--- docs/config_2.js
+++ docs/config_2.js
@@ -1,4 +1,162 @@
 window.config2 = {
     "date": "2024.06",
-    "entries": []
+    "entries": [
+        {
+            "quadrant": window.QUADRANT_BOTTOM_RIGHT,
+            "ring": window.RING_ADOPT,
+            "label": "Ruby",
+            "active": true,
+            "moved": 0
+        },
+        {
+            "quadrant": window.QUADRANT_BOTTOM_RIGHT,
+            "ring": window.RING_ADOPT,
+            "label": "Rust",
+            "active": true,
+            "moved": 0
+        },
+        {
+            "quadrant": window.QUADRANT_BOTTOM_RIGHT,
+            "ring": window.RING_TRIAL,
+            "link": "https://go.dev/",
+            "label": "Go",
+            "active": true,
+            "moved": 0
+        },
+        {
+            "quadrant": window.QUADRANT_BOTTOM_RIGHT,
+            "ring": window.RING_ADOPT,
+            "label": "Javascript",
+            "active": true,
+            "moved": 0
+        },
+        {
+            "quadrant": window.QUADRANT_BOTTOM_RIGHT,
+            "ring": window.RING_ADOPT,
+            "label": "TypeScript",
+            "active": true,
+            "moved": 0
+        },
+        {
+            "quadrant": window.QUADRANT_BOTTOM_RIGHT,
+            "ring": window.RING_ADOPT,
+            "label": "Java",
+            "active": true,
+            "moved": 0
+        },
+        {
+            "quadrant": window.QUADRANT_BOTTOM_RIGHT,
+            "ring": window.RING_ADOPT,
+            "label": "SQL",
+            "active": true,
+            "moved": 0
+        },
+        {
+            "quadrant": window.QUADRANT_BOTTOM_RIGHT,
+            "ring": window.RING_ADOPT,
+            "label": "HTML+CSS",
+            "active": true,
+            "moved": 0
+        },
+        {
+            "quadrant": window.QUADRANT_BOTTOM_LEFT,
+            "ring": window.RING_ADOPT,
+            "link": "https://puma.io",
+            "label": "Puma",
+            "active": true,
+            "moved": 0
+        },
+        {
+            "quadrant": window.QUADRANT_BOTTOM_LEFT,
+            "ring": window.RING_ADOPT,
+            "link": "https://docker.io",
+            "label": "Docker",
+            "active": true,
+            "moved": 0
+        },
+        {
+            "quadrant": window.QUADRANT_BOTTOM_LEFT,
+            "ring": window.RING_ADOPT,
+            "label": "Grafana",
+            "active": true,
+            "moved": 2
+        },
+        {
+            "quadrant": window.QUADRANT_BOTTOM_LEFT,
+            "ring": window.RING_ADOPT,
+            "label": "WireGuard",
+            "active": true,
+            "moved": 0
+        },
+        {
+            "quadrant": window.QUADRANT_BOTTOM_LEFT,
+            "ring": window.RING_ADOPT,
+            "label": "Kibana",
+            "active": true,
+            "moved": 0
+        },
+        {
+            "quadrant": window.QUADRANT_BOTTOM_LEFT,
+            "ring": window.RING_HOLD,
+            "label": "RemoteLog (rlog)",
+            "active": true,
+            "moved": 0
+        },
+        {
+            "quadrant": window.QUADRANT_BOTTOM_LEFT,
+            "ring": window.RING_HOLD,
+            "label": "HAProxy",
+            "active": true,
+            "moved": 0
+        },
+        {
+            "quadrant": window.QUADRANT_BOTTOM_LEFT,
+            "ring": window.RING_HOLD,
+            "label": "nginx",
+            "active": true,
+            "moved": 0
+        },
+        {
+            "quadrant": window.QUADRANT_TOP_LEFT,
+            "ring": window.RING_ADOPT,
+            "label": "Redis",
+            "active": true,
+            "moved": 0
+        },
+        {
+            "quadrant": window.QUADRANT_TOP_LEFT,
+            "ring": window.RING_ADOPT,
+            "label": "ElasticSearch",
+            "active": true,
+            "moved": 0
+        },
+        {
+            "quadrant": window.QUADRANT_TOP_LEFT,
+            "ring": window.RING_ADOPT,
+            "label": "Min.IO",
+            "active": true,
+            "moved": 0
+        },
+        {
+            "quadrant": window.QUADRANT_TOP_LEFT,
+            "ring": window.RING_ADOPT,
+            "label": "Prometheus",
+            "active": true,
+            "moved": 2
+        },
+        {
+            "quadrant": window.QUADRANT_TOP_LEFT,
+            "ring": window.RING_TRIAL,
+            "label": "ClickHouse",
+            "active": true,
+            "moved": 2
+        },
+        {
+            "quadrant": window.QUADRANT_TOP_LEFT,
+            "ring": window.RING_HOLD,
+            "label": "MySQL",
+            "active": true,
+            "moved": 0
+        },
+    ]
 }
