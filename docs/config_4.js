82a9b079998803394556e59fbea47f96eb82f1b7
Alexander Klump <alexander.klump@tngtech.com>
2024-09-04 11:49:27 +0200

    entwicklung-004

--- docs/config_4.js
+++ docs/config_4.js
@@ -1,4 +1,130 @@
 window.config4 = {
     "date": "2024.06",
-    "entries": []
+    "entries": [{
+        "quadrant": window.QUADRANT_BOTTOM_RIGHT,
+        "ring": window.RING_ADOPT,
+        "label": "Ruby",
+        "active": true,
+        "moved": 0
+    },{
+        "quadrant": window.QUADRANT_BOTTOM_RIGHT,
+        "ring": window.RING_HOLD,
+        "label": "CoffeeScript",
+        "active": true,
+        "moved": -1
+    },{
+        "quadrant": window.QUADRANT_BOTTOM_RIGHT,
+        "ring": window.RING_ADOPT,
+        "label": "Python",
+        "active": true,
+        "moved": 2
+    },{
+        "quadrant": window.QUADRANT_BOTTOM_RIGHT,
+        "ring": window.RING_ADOPT,
+        "label": "SQL",
+        "active": true,
+        "moved": 0
+    },{
+        "quadrant": window.QUADRANT_BOTTOM_LEFT,
+        "ring": window.RING_ADOPT,
+        "label": "Docker",
+        "active": true,
+        "moved": 0
+    },{
+        "quadrant": window.QUADRANT_TOP_RIGHT,
+        "ring": window.RING_TRIAL,
+        "label": "Superset",
+        "active": true,
+        "moved": 2
+    },{
+        "quadrant": window.QUADRANT_TOP_LEFT,
+        "ring": window.RING_ADOPT,
+        "label": "Redis",
+        "active": true,
+        "moved": 0
+    },{
+        "quadrant": window.QUADRANT_TOP_RIGHT,
+        "ring": window.RING_ADOPT,
+        "label": "DBT",
+        "active": true,
+        "moved": 2
+    },{
+        "quadrant": window.QUADRANT_TOP_RIGHT,
+        "ring": window.RING_ASSES,
+        "label": "Dagster",
+        "active": true,
+        "moved": 2
+    },{
+        "quadrant": window.QUADRANT_TOP_LEFT,
+        "ring": window.RING_TRIAL,
+        "label": "Clickhouse",
+        "active": true,
+        "moved": 2
+    },{
+        "quadrant": window.QUADRANT_TOP_LEFT,
+        "ring": window.RING_HOLD,
+        "label": "PostgreSQL",
+        "active": true,
+        "moved": 2
+    },{
+        "quadrant": window.QUADRANT_TOP_LEFT,
+        "ring": window.RING_ADOPT,
+        "label": "MinIO",
+        "active": true,
+        "moved": 0
+    },{
+        "quadrant": window.QUADRANT_TOP_LEFT,
+        "ring": window.RING_ADOPT,
+        "label": "ElasticSearch",
+        "active": true,
+        "moved": 0
+    },{
+        "quadrant": window.QUADRANT_TOP_LEFT,
+        "ring": window.RING_HOLD,
+        "label": "PostgreSQL",
+        "active": true,
+        "moved": 2
+    },{
+        "quadrant": window.QUADRANT_BOTTOM_LEFT,
+        "ring": window.RING_ADOPT,
+        "label": "HAProxy",
+        "active": true,
+        "moved": 0
+    },{
+        "quadrant": window.QUADRANT_BOTTOM_LEFT,
+        "ring": window.RING_ADOPT,
+        "label": "Wireguard",
+        "active": true,
+        "moved": 0
+    },{
+        "quadrant": window.QUADRANT_BOTTOM_LEFT,
+        "ring": window.RING_TRIAL,
+        "label": "Terraform",
+        "active": true,
+        "moved": 2
+    },{
+        "quadrant": window.QUADRANT_BOTTOM_RIGHT,
+        "ring": window.RING_ADOPT,
+        "label": "TypeScript",
+        "active": true,
+        "moved": 0
+    },{
+        "quadrant": window.QUADRANT_BOTTOM_LEFT,
+        "ring": window.RING_ADOPT,
+        "label": "Nginx",
+        "active": true,
+        "moved": 0
+    },{
+        "quadrant": window.QUADRANT_BOTTOM_LEFT,
+        "ring": window.RING_ADOPT,
+        "label": "Prometheus",
+        "active": true,
+        "moved": 0
+    },{
+        "quadrant": window.QUADRANT_TOP_RIGHT,
+        "ring": window.RING_ADOPT,
+        "label": "Grafana",
+        "active": true,
+        "moved": 0
+    }]
 }
