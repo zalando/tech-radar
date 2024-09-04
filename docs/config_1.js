2b2b8a995c892fcae43121d99210d8038b2fdb6c
Patrick Romowicz <patrick@romowicz.de>
2024-09-04 10:53:19 +0200

    first examples

--- docs/config_1.js
+++ docs/config_1.js
@@ -2,11 +2,23 @@ window.config1 = {
     "date": "2024.06",
     "entries": [
     {
-        "quadrant": window.QUADRANT_TOP_RIGHT,
-        "ring": window.RING_TRIAL,
-        "label": "TEST! :)",
+        "quadrant": window.QUADRANT_BOTTOM_RIGHT,
+        "ring": window.RING_ADOPT,
+        "label": "PHP",
         "active": true,
-        "moved": 1
+        "moved": 0
+    },{
+        "quadrant": window.QUADRANT_TOP_LEFT,
+        "ring": window.RING_HOLD,
+        "label": "Percona XtraDB Cluster",
+        "active": true,
+        "moved": 0
+    },{
+        "quadrant": window.QUADRANT_TOP_LEFT,
+        "ring": window.RING_TRIAL,
+        "label": "TiDB",
+        "active": true,
+        "moved": 0
     },
 ]
 }
